69f61c08fa7e7a7a5960608d9ebf037587fdff35
Jinkyou Son <nallwhy@gmail.com>
2023-10-16 12:31:11 +0900

    Config drizzle
    -a

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from "./schema"
 
 const queryClient = postgres(process.env.DATABASE_URL!);
 
-export const db: PostgresJsDatabase = drizzle(queryClient, { schema: schema, logger: true });
+export const db: PostgresJsDatabase = drizzle(queryClient, { logger: true });
