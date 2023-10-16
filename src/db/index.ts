1862ba3e500bc847ec9fcf9974dc53ee23011c90
Jinkyou Son <nallwhy@gmail.com>
2023-10-16 12:47:58 +0900

    Setup db migration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,9 @@
 import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
 const queryClient = postgres(process.env.DATABASE_URL!);
 
 export const db: PostgresJsDatabase = drizzle(queryClient, { logger: true });
+
+await migrate(db, { migrationsFolder: './migrations' });
