bb7f45e15c3fe3a87e8aad7cb9a443e7ff3c63ed
Jinkyou Son <nallwhy@gmail.com>
2023-11-11 15:56:04 +0900

    Refactor jsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,13 @@ import { Todo } from './domain/todos/todo';
 
 const app = new Elysia()
   .use(html())
-  .get('/', ({ html }) =>
-    html(
-      <BaseHtml>
-        <div class="flex w-full h-screen justify-center items-center">
-          <div hx-get="/todos" hx-trigger="load" hx-swap="innerHTML"></div>
-        </div>
-      </BaseHtml>
-    )
-  )
+  .get('/', () => (
+    <BaseHtml>
+      <div class="flex w-full h-screen justify-center items-center">
+        <div hx-get="/todos" hx-trigger="load" hx-swap="innerHTML"></div>
+      </div>
+    </BaseHtml>
+  ))
   .get('/todos', async () => {
     const todos: Todo[] = await listTodos();
 
@@ -40,18 +38,15 @@ console.log(
   `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
 
-const BaseHtml = ({ children }: elements.Children) => `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>THE BETH STACK</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script src="https://unpkg.com/htmx.org@1.9.3"></script>
-</head>
-<body>
-  ${children}
-</body>
-</html>
-`;
+const BaseHtml = ({ children }: elements.Children) => (
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>THE BETH STACK</title>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script src="https://unpkg.com/htmx.org@1.9.3"></script>
+    </head>
+    <body>{children}</body>
+  </html>
+);
