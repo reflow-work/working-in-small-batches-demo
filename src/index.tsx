0812d4405324a129d2545fc893ebc09203a8d256
Jinkyou Son <nallwhy@gmail.com>
2023-11-11 16:21:04 +0900

    Setup hyperscript

--- src/index.tsx
+++ src/index.tsx
@@ -75,6 +75,7 @@ const BaseHtml = ({ children }: elements.Children) => (
       <title>THE BETH STACK</title>
       <script src="https://cdn.tailwindcss.com"></script>
       <script src="https://unpkg.com/htmx.org@1.9.3"></script>
+      <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
     </head>
     <body>{children}</body>
   </html>
