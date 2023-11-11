ee2d935e1c180ee8acdbe44e9d1b9eba10f77628
Jinkyou Son <nallwhy@gmail.com>
2023-11-11 15:35:26 +0900

    Apply completed status to TodoItem component

--- src/components/todo.component.tsx
+++ src/components/todo.component.tsx
@@ -1,11 +1,11 @@
 import { Todo } from '../domain/todos/todo';
 import * as elements from 'typed-html';
 
-function TodoItem({ content, completed, id }: Todo) {
+function TodoItem({ content, completed }: Todo) {
   return (
     <div class="flex flex-row space-x-3">
       <p>{content}</p>
-      <input type="checkbox" />
+      <input type="checkbox" checked={completed} />
       <button class="text-red-500">X</button>
     </div>
   );
